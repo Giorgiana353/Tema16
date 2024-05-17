37e1bd5162dafb867bb673c4c69bc7fdf32b06ae
Giorgiana353 <146415215+Giorgiana353@users.noreply.github.com>
2024-05-17 20:18:27 +0300

    Create Message.java

---
+++ Message.java
@@ -0,0 +1,27 @@
+
+public class Message {
+	
+	private Person sender;
+	private Person receiver;
+	private String content;
+	
+	public Message(Person sender, Person receiver, String content) {
+		super();
+		this.sender = sender;
+		this.receiver = receiver;
+		this.content = content;
+	}
+
+	public Person getSender() {
+		return sender;
+	}
+
+	public Person getReceiver() {
+		return receiver;
+	}
+
+	public String getContent() {
+		return content;
+	}
+	
+}
